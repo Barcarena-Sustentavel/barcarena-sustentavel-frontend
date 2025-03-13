4644b41ae5ab0bb8e848e18e4a8b9c4d921fefdf
Marrior-dot <mrlimaroberto@gmail.com>
2025-03-12 23:57:15 -0300

    realizando testes no front

---
+++ src/tests/indicador/post.test.tsx
@@ -0,0 +1,4 @@
+import { postIndicador } from "../../components/admin/create/aux/indicador/postIndicador";
+import { GraficosIndicador } from "../../interfaces/indicador_interface";
+import { render, fireEvent, waitFor } from '@testing-library/react';
+
