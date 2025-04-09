6e95c59025ced46a3fe7cfea47256ed30d465b34
Marrior-dot <mrlimaroberto@gmail.com>
2025-04-09 13:50:02 -0300

    arquivos da build modificados, nginx modificado e api.tsx também modificado

--- src/api.tsx
+++ src/api.tsx
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://0.0.0.0:8081",
+  //baseURL: "http://3.148.173.110:8081/",
+  //baseURL: "http://backend:8081/",
+  baseURL:"/api/"
+
 });
 
 export default api;
