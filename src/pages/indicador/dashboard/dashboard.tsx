1d5b5dc8cb3706cee157d9a88f13e5c43e92200e
Marrior-dot <mrlimaroberto@gmail.com>
2025-05-03 22:17:46 -0300

    modificados os dashboards para acomodar outros tipos de gráficos

--- src/pages/indicador/dashboard/dashboard.tsx
+++ src/pages/indicador/dashboard/dashboard.tsx
@@ -1,67 +1,97 @@
-import * as Highcharts from 'highcharts';
-import HighchartsReact from 'highcharts-react-official';
-import HighchartsMore from 'highcharts/highcharts-more';
-import { FC, useRef } from 'react';
+import * as Highcharts from "highcharts";
+import HighchartsReact from "highcharts-react-official";
+import HighchartsMore from "highcharts/highcharts-more";
+import { FC, useRef } from "react";
 
 interface PlotSeries {
-    name: string;
-    data: number[];
+  name: string;
+  data: number[];
+}
+
+interface TreeMapSeries {
+  name: string;
+  value: number;
 }
 
 interface DashboardProps {
-    tipoGrafico: string;
-    tituloGrafico: string | null;
-    dados: PlotSeries[];
-    categorias: string[] | number[];
+  tipoGrafico: string;
+  tituloGrafico: string | null;
+  dados: PlotSeries[];
+  categorias: string[] | number[];
 }
 
 const plotOptions = (dashboard: DashboardProps) => {
-    return {
-        chart: {
+  const seriesDados =
+    dashboard.tipoGrafico === "treemap"
+      ? [
+          {
             type: dashboard.tipoGrafico,
-        },
-        title: {
-            text: dashboard.tituloGrafico ?? '',
-        },
-        series: dashboard.dados,
-        xAxis: {
-            categories: dashboard.categorias,
-        },
-        yAxis: {
-            title: {
-                text: 'Valores',
-            },
-        },
-    };
+            layoutAlgorithm: "squarified",
+            data: dashboard.dados,
+          },
+        ]
+      : dashboard.dados;
+
+  return {
+    chart: {
+      type: dashboard.tipoGrafico,
+    },
+    title: {
+      text: dashboard.tituloGrafico ?? "",
+    },
+    //series: dashboard.dados,
+    series: seriesDados,
+    xAxis: {
+      categories: dashboard.categorias,
+    },
+    yAxis: {
+      title: {
+        text: "Valores",
+      },
+    },
+  };
 };
 
 export const DashboardComponent: FC<{
-    tipoGrafico: string;
-    dados: number[][];
-    colunas: string[];
-    tituloGrafico: string | null;
-    categorias: string[] | number[];
-}> = ({ tipoGrafico, dados, tituloGrafico, categorias,colunas }) => {
-    const chartComponentRef = useRef<HighchartsReact.RefObject>(null);
+  tipoGrafico: string;
+  dados: number[][];
+  colunas: string[];
+  tituloGrafico: string | null;
+  categorias: string[] | number[];
+}> = ({ tipoGrafico, dados, tituloGrafico, categorias, colunas }) => {
+  const chartComponentRef = useRef<HighchartsReact.RefObject>(null);
 
-    const dadosGrafico: PlotSeries[] = [];
+  const dadosGraficoPlots: PlotSeries[] = [];
+  const dadosGraficosTrees: TreeMapSeries[] = [];
+  let finalDadosGraficos: any[] = [];
+  if (tipoGrafico === "treemap") {
+    for (let i = 0; i < dados.length; i++) {
+      dadosGraficosTrees.push({
+        name: colunas[i], //`Dado ${index + 1}`,
+        value: dados[i][0],
+      });
+    }
+    finalDadosGraficos = dadosGraficosTrees;
+  } else {
     dados.forEach((dado, index) => {
-        dadosGrafico.push({
-            name: colunas[index],//`Dado ${index + 1}`,
-            data: dado,
-        });
+      dadosGraficoPlots.push({
+        name: colunas[index], //`Dado ${index + 1}`,
+        data: dado,
+      });
     });
+    finalDadosGraficos = dadosGraficoPlots;
+  }
 
-    return (
-        <HighchartsReact
-            highcharts={Highcharts}
-            options={plotOptions({
-                tipoGrafico,
-                dados: dadosGrafico,
-                tituloGrafico,
-                categorias,
-            })}
-            ref={chartComponentRef}
-        />
-    );
+  return (
+    <HighchartsReact
+      highcharts={Highcharts}
+      options={plotOptions({
+        tipoGrafico,
+        dados: finalDadosGraficos,
+        tituloGrafico,
+        categorias,
+      })}
+      ref={chartComponentRef}
+    />
+  );
 };
