358a63eb7553de68b7113dc4272c36490cf28de4
Victor <seuemail@exemplo.com>
2025-09-04 16:54:30 -0300

    envio de email adicionado à contribuição

--- src/hooks/useContribuicao.ts
+++ src/hooks/useContribuicao.ts
@@ -1,5 +1,7 @@
 import api from '../api.tsx';
 import axios from 'axios';
+import emailjs from "@emailjs/browser";
+import { useLocation } from "react-router-dom";
 
 interface SubmitContribuicaoParams {
   formData: FormData;
@@ -8,21 +10,56 @@ interface SubmitContribuicaoParams {
 }
 
 export const useContribuicao = () => {
+  async function enviarEmail(form: FormData){
+    
+    const SERVICE_ID = import.meta.env.VITE_EMAILJS_SERVICE_ID;
+    const TEMPLATE_ID = import.meta.env.VITE_EMAILJS_TEMPLATE_ID;
+    const PUBLIC_KEY = import.meta.env.VITE_EMAILJS_PUBLIC_KEY;
+
+    const formParameters = {
+      nome: form.get('nome'),
+      email: form.get('email'),
+      telefone: form.get('telefone'),
+      comentario: form.get('formulario')
+    }
+
+    try{
+      const resultado = await emailjs.send(
+      SERVICE_ID, TEMPLATE_ID, formParameters, {publicKey: PUBLIC_KEY}
+      );
+
+      alert("E-mail enviado com sucesso!");
+    } catch (err) {
+      console.error("Erro:", err);
+      alert("Erro ao enviar a mensagem.");
+    }
+    
+  }
+
+  function getPathAtual(){
+    const pathAtual = useLocation().pathname;
+
+    // retira acentos da string do path atual
+    return pathAtual.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+  }
+
   const submitContribuicao = async ({ 
     formData, 
     onSuccess, 
     onError 
   }: SubmitContribuicaoParams) => {
     try {
-      await api.post('/contribuicao', formData, {
+      alert(getPathAtual());
+      await api.post(`/dimensoes/contribuicao/${getPathAtual()}/`, formData, {
         headers: {
           'Content-Type': 'multipart/form-data',
         }
       });
+      enviarEmail(formData);
       onSuccess?.();
     } catch (error) {
         let userFriendlyMessage = 'Não foi possível enviar sua contribuição';
-  
+
         if (axios.isAxiosError(error)) {
             userFriendlyMessage = error.response?.data?.message || 
                                 getCustomMessage(error.response?.status) ||
