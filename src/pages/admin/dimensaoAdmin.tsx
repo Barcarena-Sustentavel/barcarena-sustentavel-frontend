299b6ee2bc5238eca3462514b35a3dcc63557c7b
Victor <seuemail@exemplo.com>
2025-11-04 14:09:30 -0300

    interface para modificação de email destinatário de contribuição implementada

--- src/pages/admin/dimensaoAdmin.tsx
+++ src/pages/admin/dimensaoAdmin.tsx
@@ -2,6 +2,7 @@ import { FC, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import dimensoes from "../../utils/const.tsx";
 import "./css/dimensaoAdmin.css";
+import { Modal, Button, Form } from "react-bootstrap";
 
 const DimensaoAdmin: FC = () => {
   const navigate = useNavigate();
@@ -12,10 +13,50 @@ const DimensaoAdmin: FC = () => {
   const { dimensoesColumn1, dimensoesColumn2, dimensoesCores12 } =
     dimensoes.GetAllConst();
 
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const [email, setEmail] = useState<string>("");
+  
+
+
   return (
     <div className="home-container">
-      <div className="admin-header-dimensao-admin">
+      <div className="position-relative d-flex align-items-center justify-content-center admin-header-dimensao-admin">
         <h1>Administração de Dimensões</h1>
+        <span className="position-absolute end-0 gear-icon"
+              onClick={handleShow}>⚙️</span>
+        <Modal show={show} onHide={handleClose} centered>
+        <Modal.Header closeButton>
+          <Modal.Title className="email-config-title">Destinatário de Contribuições</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group className="mb-3">
+              <Form.Label className="email-current-title">E-mail Atual</Form.Label>
+              <Form.Control
+                type="text"
+                name="nome"
+                value={email}
+                onChange={(e) => {setEmail}}
+                placeholder="Digite um E-mail"
+              />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Modificar E-mail
+            </Button>
+
+
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Fechar
+          </Button>
+        </Modal.Footer>
+      </Modal>
       </div>
       <div className="dimensoes-grid-wallpaper">
         <div className="dimensoes-grid">
