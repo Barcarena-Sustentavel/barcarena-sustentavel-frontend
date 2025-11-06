c0fe618646404fe340009f62861c3794acff22b0
Victor <seuemail@exemplo.com>
2025-11-06 13:18:06 -0300

    fix: typo na importação de backButton.css

--- src/components/layout/backButton/backButton.tsx
+++ src/components/layout/backButton/backButton.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import "./BackButton.css";
+import "./backButton.css";
 
 export default function BackButton() {
   const navigate = useNavigate();
